343d537d3bc0b242b47d612c9c7c4f691a82edf8
Mahbubur Rahman <blackmahbub@gmail.com>
2018-02-22 19:05:08 +0100

    display walking direction from university to home

--- GoogleMapNavigationApp/GoogleMapNavigationApp/ViewController.swift
+++ GoogleMapNavigationApp/GoogleMapNavigationApp/ViewController.swift
@@ -7,42 +7,39 @@
 //
 
 import UIKit
-//import CoreLocation
+import CoreLocation
 import GoogleMaps
 import GooglePlaces
 import GooglePlacePicker
 
-class ViewController: UIViewController { //, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-//    let locationManager = CLLocationManager()
+    let locationManager = CLLocationManager()
     let universityCampusArea = CLLocationCoordinate2D(latitude: 50.5665016, longitude: 9.6855674)
     
-//    var isRegionDefined  = false
+    var googleDirection: GoogleDirection? = nil
     
     override func loadView() {
         
         let cameraPostion = GMSCameraPosition.camera(withLatitude: universityCampusArea.latitude, longitude: universityCampusArea.longitude, zoom: 18)
         let mapView = GMSMapView.map(withFrame: .zero, camera: cameraPostion)
-        
-        mapView.isIndoorEnabled = true
+
         mapView.isMyLocationEnabled = true
         
+        
         self.view = mapView
-        
-        self.getDirectionFromGoogleMapAPI()
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        locationManager.delegate = self
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-//        locationManager.activityType = CLActivityType.otherNavigation
-//        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.activityType = CLActivityType.otherNavigation
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,57 +47,103 @@ class ViewController: UIViewController { //, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func getDirectionFromGoogleMapAPI() {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        URLSession.shared.dataTask(with: URL(string: "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=AIzaSyA5WKLZCTreqWGGVNdeucTzqCgsLfEf8CU")!) {
-
-            (data, response, error) in
+        let currentLocation = locations.last!
+        print("\nMy Location from location manager: \(currentLocation)\n")
+        
+//        let cameraPostion = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 18)
+//
+//        let mapView = GMSMapView.map(withFrame: .zero, camera: cameraPostion)
+//        mapView.isMyLocationEnabled = true
+//
+//        self.view = mapView
+        
+        let origin = currentLocation.coordinate
+        let destination = CLLocationCoordinate2D(latitude: 50.5551995, longitude: 9.6793356)
+        self.getDirectionFromGoogleMapAPI(origin: origin, destination: destination)
+    }
+    
+    func getDirectionFromGoogleMapAPI(origin: CLLocationCoordinate2D, destination: CLLocationCoordinate2D) {
+        
+        let url = URL(string: "https://maps.googleapis.com/maps/api/directions/json?origin=\(origin.latitude),\(origin.longitude)&destination=\(destination.latitude),\(destination.longitude)&key=AIzaSyA5WKLZCTreqWGGVNdeucTzqCgsLfEf8CU&mode=walking")!
+        
+        print("\nURL: \(url)\n")
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
 
             print("\nJSON Response from Google Direction API:\n")
             print("\(data)\n")
+            
+            guard let directionData = data else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                
+                do {
+                    self.googleDirection = try JSONDecoder().decode(GoogleDirection.self, from: directionData)
+                    print("After Decode: \(self.googleDirection)\n")
+                } catch let jsonError {
+                    print("\nJSON Error: " + jsonError.localizedDescription + "\n")
+                }
+                
+                guard self.googleDirection != nil,
+                    "OK" == self.googleDirection!.status else {
+                        
+                        return
+                }
+                
+                print("\nCreating GMS Path ...\n")
+                let steps = self.googleDirection!.routes.flatMap({ $0.legs.flatMap({ $0.steps }) })
+                print("\nSteps: \(steps)\n")
+                
+                let path = GMSMutablePath()
+                steps.forEach { step in
+                    
+                    path.add(CLLocationCoordinate2D(latitude: step.start_location.lat, longitude: step.start_location.lng))
+                    path.add(CLLocationCoordinate2D(latitude: step.end_location.lat, longitude: step.end_location.lng))
+                }
+                
+                let polyline = GMSPolyline(path: path)
+                polyline.strokeWidth = 5
+                polyline.strokeColor = UIColor.purple
+                polyline.map = self.view as! GMSMapView
+            }
         }.resume()
     }
-
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//
-//        let currentLocation = locations.last!
-//
-//        if !isRegionDefined {
-//
-//            let cameraPostion = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 18)
-//            let mapView = GMSMapView.map(withFrame: .zero, camera: cameraPostion)
-//
-//            mapView.isMyLocationEnabled = true
-//
-//            self.view = mapView
-//
-//            isRegionDefined = true
-//        }
-//    }
-
+    
 }
 
 struct GoogleDirection: Decodable {
     
-    
-    
+    var routes: [GoogleRoute]
+    var status: String
 }
 
 struct GoogleRoute: Decodable {
     
-    
+    var legs: [GoogleLeg]
     
 }
 
 struct GoogleLeg: Decodable {
     
-    
+    var steps: [GoogleStep]
     
 }
 
 struct GoogleStep: Decodable {
     
+    var start_location: GoogleLocation
+    var end_location: GoogleLocation
     
+}
+
+struct GoogleLocation: Decodable {
+    
+    var lat: Double
+    var lng: Double
     
 }
 
